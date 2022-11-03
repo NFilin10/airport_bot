ab7e69233c7fe7bdfd323679881ca4e6709261e8
Nikita Filin <nikita10filin@gmail.com>
2022-11-03 11:37:42 +0200

    country indexes func

--- main.py
+++ main.py
@@ -24,3 +24,18 @@ def get_all_departures():
         y += 5
     print(b)
 
+#получение индексов каждой страны, чтобы можно было определять какая страна интересует пользователя
+def get_country_indexes():
+    url = "https://www.tallinn-airport.ee/lennuinfo/sihtkohad/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    ids = soup.find_all('li', class_='destination-item')
+
+    country_data = {}
+    for id in ids:
+        country_name = id.text.strip()
+        country_id = id.get('data-destination')
+        country_data[country_name] = country_id
+    return country_data
+
+
